0d965eaefd67b44615cd776b7ff730cf1203fb9a
masprog2022 <masprog2022@gmail.com>
2024-04-25 00:13:58 +0100

    first execution on server

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const express = require("express");
 
 const app = express();
+
+app.get("/", (request, response) => {
+  response.send("Hello, World!");
+});
+
+app.listen(3001, () => {
+  console.log("Server running on port 3001");
+});
